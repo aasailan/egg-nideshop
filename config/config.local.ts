1f9b2df45aa4d2e6f24e29650f4355de49eb4d03
qiao <184443035@qq.com>
2018-07-23 19:43:53 +0800

    添加readme文件

--- config/config.local.ts
+++ config/config.local.ts
@@ -11,8 +11,17 @@ export default () => {
     port: '3306',
     username: 'root',
     password: '12345678',
-
+    // 数据表名前缀
     tablePrefix: 'nideshop_',
+  };
+
+  // 微信配置
+  config.wechat = {
+    appid: 'wx7dffdfe1be046be9',
+    secret: 'b174075db7ef3cf7119b43fdc6f72e9a',
+    mch_id: '',
+    partner_key: '',
+    notify_url: '',
   };
 
   config.cors = {
